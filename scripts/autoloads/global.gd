bc3b98524ae249888d851794b2e21524bc53fdd1
Nimajjj <benjamin.borello@aol.com>
2023-11-15 11:38:17 +0100

    chore(script): add global autoload script

---
+++ scripts/autoloads/global.gd
@@ -0,0 +1,10 @@
+## File:	autoloads/global.gd
+## Store global variables.
+##
+## Created: 15/11/2023
+##
+## <!> Please ask your referent before update this script.
+## <!> Be careful before query global variable.
+## <!> Always checks var != null
+extends Node
+
