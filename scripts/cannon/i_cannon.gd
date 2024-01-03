5f454c8e702d7213493789329bd1fba68ec1adc5
Bahsiik <o.mist1303@gmail.com>
2024-01-03 15:49:49 +0100

    feat(ICannon): Add test cannon shot on the mouse position.

--- scripts/cannon/i_cannon.gd
+++ scripts/cannon/i_cannon.gd
@@ -5,19 +5,21 @@ extends Node2D
 @export var range: float
 @export var fire_rate: float
 
+@export var BulletScene: PackedScene = null
+
+
 func upgrade():
 	pass # IUpgrade dans argument
 
 func fire():
-	pass
-
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+	if Input.is_action_just_pressed("fire"):
+		var bullet_instance: IBullet = BulletScene.instantiate()
+		bullet_instance.position = position
+		bullet_instance.direction = (get_viewport().get_mouse_position() - position).normalized()
+		bullet_instance.rotation = bullet_instance.direction.angle()
+		bullet_instance.target = get_viewport().get_mouse_position()
+		add_child(bullet)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	fire()
