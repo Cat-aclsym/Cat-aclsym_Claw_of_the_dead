3a57dbab19c26aa6f322208d4da9e963881923fd
Plcuf <romeo.sanchez@ynov.com>
2024-01-03 09:52:44 +0100

    feat(cannon):Add the base of Icannon

---
+++ scripts/cannon/i_cannon.gd
@@ -0,0 +1,23 @@
+class_name ICannon
+extends Node2D
+
+@export var bullet: IBullet
+@export var range: float
+@export var fire_rate: float
+
+func upgrade():
+	pass # IUpgrade dans argument
+
+func fire():
+	pass
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
