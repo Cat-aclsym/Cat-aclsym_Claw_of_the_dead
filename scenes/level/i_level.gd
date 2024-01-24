3634a49fffbf8febe2204cf0f8dc3d6dc605f878
Nimajjj <benjamin.borello@aol.com>
2024-01-24 15:17:19 +0100

    chore(map): add ILevel, LevelMetadata and LevelDebug scenes

---
+++ scenes/level/i_level.gd
@@ -0,0 +1,37 @@
+class_name ILevel
+extends Node2D
+
+var waves: Array[String] = []
+var currentWave: int = 1
+
+@export var map_scene: PackedScene = null
+
+@onready var Metadata: LevelMetadata = $LevelMetadata
+
+
+func _ready() -> void:
+	Log.info("Loading level [{0}] {1}".format([Metadata.id, Metadata.level_name]))
+	
+	_load_map()
+
+
+func startNewWave() -> void:
+	pass
+
+
+func win() -> void:
+	pass
+
+
+func loose() -> void:
+	pass
+
+
+func _load_map() -> void:
+	Log.info("Loading map...")
+	if !map_scene:
+		Log.error("An IMap must be provided to create a level!")
+		return
+		
+	var map: IMap = map_scene.instantiate()
+	add_child(map)
