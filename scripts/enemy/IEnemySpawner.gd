339e9d6615275658c79395c4c87ee9a1948eb064
antaww <anta.pizzetta@gmail.com>
2024-01-24 14:29:20 +0100

    fix(enemy): Add missing godot changes.

--- scripts/enemy/IEnemySpawner.gd
+++ scripts/enemy/IEnemySpawner.gd
@@ -5,7 +5,7 @@ var zombie_types: Array = ['default']
 
 func _ready():
 	var enemy: IEnemy = default_zombie_scene.instantiate()
-	var path: Path2D = get_node('Path2D')
+	var path: Path2D = get_node('Path2D') # set the path name to where you want to go
 	var path_follow: PathFollow2D = path.get_node('PathFollow2D')
 	enemy.path = path_follow
 	spawn_enemy(path_follow, enemy)
