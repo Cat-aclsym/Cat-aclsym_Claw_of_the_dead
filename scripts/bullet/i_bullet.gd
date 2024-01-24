6fc710ba386182c50d2b83930673d6670d97a9b1
Ayfri <pierre.ayfri@gmail.com>
2024-01-24 15:02:00 +0100

    fix(changes): Fix missing changes.

--- scripts/bullet/i_bullet.gd
+++ scripts/bullet/i_bullet.gd
@@ -1,9 +1,9 @@
 class_name IBullet
 extends Area2D
 
+
 @export var speed: int
 @export var damage: int
-
 @export var direction: Vector2
 @export var target: Vector2
 
@@ -11,11 +11,13 @@ extends Area2D
 func _init():
 	pass
 
-func _physics_process(delta: float):
+
+func _physics_process(delta):
 	position += direction * speed * delta
-	
+
 	if global_position.distance_to(target) < 10:
 		queue_free()
+
 
 func _on_area_entered(area):
 	pass
--- scripts/cannon/i_cannon.gd
+++ scripts/cannon/i_cannon.gd
@@ -1,64 +1,63 @@
 class_name ICannon
 extends Node2D
 
+
 @export var bullet: IBullet
 @export var range: float
 @export var fire_rate: float
 @export var BulletScene: PackedScene = null
 
-@onready var fire_rate_timer := $FireRateTimer as Timer
-
 var enemy_array: Array[IEnemy]
+
 
 func upgrade():
 	pass # IUpgrade dans argument
 
+
 func fire():
 	if !len(enemy_array):
 		return
-		
+
 	if BulletScene == null:
 		return
-		
-		
-	var enemy_position: Vector2 = enemy_array[0].global_position
+
+	var enemy_position: Vector2  = enemy_array[0].global_position
 	var bullet_instance: IBullet = BulletScene.instantiate()
-	
-	
+
 	bullet_instance.direction = global_position.direction_to(enemy_position)
 	bullet_instance.rotation = bullet_instance.direction.angle()
 	bullet_instance.target = enemy_position
 	add_child(bullet_instance)
-	fire_rate_timer.start()
-	
-	
-		
-		
-		
-		
+
+
+
+
+
+
 	#if Input.is_action_just_pressed("fire") and :
-		#if BulletScene == null: return
-		#var mouse_position: Vector2 = get_global_mouse_position()
-		#var bullet_instance: IBullet = BulletScene.instantiate()
-		#bullet_instance.direction = (mouse_position - global_position).normalized()
-		#bullet_instance.rotation = bullet_instance.direction.angle()
-		#bullet_instance.target = mouse_position
-		#add_child(bullet_instance)
-		#
-		#bullet_instance.global_position = global_position
-		#
-		#Log.debug("global_position:")
-		#Log.debug(global_position)
-		#
-		#Log.debug("mouse_position")
-		#Log.debug(mouse_position)
-#
-		#Log.debug("bullet_instance.global_position")
-		#Log.debug(bullet_instance.global_position)
+	#if BulletScene == null: return
+	#var mouse_position: Vector2 = get_global_mouse_position()
+	#var bullet_instance: IBullet = BulletScene.instantiate()
+	#bullet_instance.direction = (mouse_position - global_position).normalized()
+	#bullet_instance.rotation = bullet_instance.direction.angle()
+	#bullet_instance.target = mouse_position
+	#add_child(bullet_instance)
+	#
+	#bullet_instance.global_position = global_position
+	#
+	#Log.debug("global_position:")
+	#Log.debug(global_position)
+	#
+	#Log.debug("mouse_position")
+	#Log.debug(mouse_position)
+	#
+	#Log.debug("bullet_instance.global_position")
+	#Log.debug(bullet_instance.global_position)
+
 
 func _process(delta):
-	if fire_rate_timer.is_stopped():
-		fire()
+	fire()
+
 
 func _on_area_2d_body_entered(body):
 	if body is IEnemy:
