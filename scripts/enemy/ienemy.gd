8767272a0755c8250fc7f98821f94daaf4198518
dantoine <antoine.debarbarin@ynov.com>
2024-02-14 14:57:48 +0100

    feat(enemy): Updated sprite direction animation v1.

--- scripts/enemy/ienemy.gd
+++ scripts/enemy/ienemy.gd
@@ -6,6 +6,13 @@ enum ENM_State {
 	GIVE_DAMAGE
 }
 
+enum ENM_Direction {
+	UP_RIGHT,
+	UP_LEFT,
+	DOWN_RIGHT,
+	DOWN_LEFT
+}
+
 enum ENM_Type {
 	DEFAULT,
 	BIG_DADDY,
@@ -14,25 +21,32 @@ enum ENM_Type {
 @export var speed: float = 30
 @export var max_health: float = 100
 @export var type: ENM_Type = ENM_Type.DEFAULT
-		
+
 var disappear_animation: String = "FadeOut"
 var walk_up_animation: String = "walk_up"
 var walk_down_animation: String = "walk_down"
 
 var health: float
 var state: ENM_State = ENM_State.FOLLOW_PATH
-var path: PathFollow2D = null
-var previous_point: Vector2
+var path_follow: PathFollow2D = null
+var path: Path2D = null
+var direction: ENM_Direction = ENM_Direction.UP_RIGHT
 var is_dead: bool:
 	get:
 		return health == 0
+var current_point_id: int = 0
+var path_points_size: int
 
 @onready var Sprite: Sprite2D = $Sprite2D
 @onready var AnimPlayer: AnimationPlayer = Sprite.get_node("AnimationPlayer")
 
+
 # core
 func _ready():
 	health = max_health
+	path_points_size = path.curve.get_baked_points().size()
+	_get_path_direction()
+	current_point_id += 1
 	AnimPlayer.connect("animation_finished", _on_animation_player_animation_finished)
 
 
@@ -47,7 +61,7 @@ func _physics_process(delta: float) -> void:
 			_give_damage_state()
 		_:
 			pass
-			
+
 	follow_path(delta)
 	#_take_damage(1)
 	#print(health)
@@ -62,37 +76,58 @@ func take_damage(damage: float) -> void:
 		health -= damage
 
 
-func check_next_point() -> bool:
-	var points: Array = path.curve.get_baked_points()
-	var current_position: Vector2 = path.position
+func _get_path_direction():
+	var x_pos_difference: float = path.curve.get_point_position(current_point_id).x - path.curve.get_point_position(current_point_id+1).x;
+	var y_pos_difference: float = path.curve.get_point_position(current_point_id).y - path.curve.get_point_position(current_point_id+1).y;
 
-	if current_position == points[path.get_closest_point_index() + 1]:
-		return true
-	else:
-		return false
+	# todo: fix image flip not working sometimes
+	if x_pos_difference > 0.:
+		if y_pos_difference < 0.:
+			direction = ENM_Direction.DOWN_RIGHT  # Moving right down
+		elif y_pos_difference > 0.:
+			direction = ENM_Direction.UP_RIGHT  # Moving right up
+	elif x_pos_difference < 0.:
+		if y_pos_difference < 0.:
+			direction = ENM_Direction.DOWN_LEFT  # Moving left down
+		elif y_pos_difference > 0.:
+			direction = ENM_Direction.UP_LEFT  # Moving left up
+
+
+func _update_direction():
+	print(current_point_id)
+	print(path_follow.position, path.curve.get_point_position(current_point_id+1))
+	if path_follow.position == path.curve.get_point_position(current_point_id+1):
+		print("changed direction")
+		_get_path_direction()
+		current_point_id += 1
 
 
 func follow_path(delta: float) -> void:
-	var x_pos_difference: float = path.position.x - previous_point.x;
-	var y_pos_difference: float = path.position.y - previous_point.y;
-	
-	# todo: fix image flip not working sometimes
-	if x_pos_difference > 0.:
-		Sprite.flip_h = false
-	elif x_pos_difference < 0.:
-		Sprite.flip_h = true
-
-	previous_point = Vector2(path.position);
-
-	if path.get_progress_ratio() >= 1:
+	if path_follow.get_progress_ratio() >= 1:
 		# Path finished
 		_give_damage_state()
 		return
-	path.set_progress(path.get_progress() + (speed * delta))
-	if y_pos_difference > 0.:
-		AnimPlayer.play(walk_up_animation)
-	elif y_pos_difference < 0.:
-		AnimPlayer.play(walk_down_animation)
+	path_follow.set_progress(path_follow.get_progress() + (speed * delta))
+	_update_direction()
+	_walk()
+
+
+func _walk():
+	match direction:
+		ENM_Direction.UP_RIGHT:
+			Sprite.flip_h = false
+			AnimPlayer.play(walk_up_animation)
+		ENM_Direction.UP_LEFT:
+			Sprite.flip_h = true
+			AnimPlayer.play(walk_up_animation)
+		ENM_Direction.DOWN_RIGHT:
+			Sprite.flip_h = false
+			AnimPlayer.play(walk_down_animation)
+		ENM_Direction.DOWN_LEFT:
+			Sprite.flip_h = true
+			AnimPlayer.play(walk_down_animation)
+		_:
+			pass
 
 
 # internal
