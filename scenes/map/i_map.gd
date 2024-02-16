d23facfd25814fa2442e62b1bd81cc283045ba44
Nimajjj <benjamin.borello@aol.com>
2024-02-16 18:56:28 +0100

    fix(ienemy): fix bug where all enemies isntances playes same animation + format random files

--- scenes/map/i_map.gd
+++ scenes/map/i_map.gd
@@ -1,42 +1,40 @@
 class_name IMap 
 extends Node2D
 
-var enemy1: PackedScene = preload("res://scenes/enemy/ienemy_scene.tscn")
+var ienemy_scene: PackedScene = preload("res://scenes/enemy/ienemy_scene.tscn")
 
 var paths: Array[Path2D] = []
+var max_spawn: int = 20
+var current_spawning: int = 0:
+	get:
+		current_spawning += 1
+		return current_spawning - 1
+		
 @onready var Paths: Node2D = $Paths
-var max_spawn: int = 2
-var current_spawning: int = 0
 
+
+# core
 func _ready() -> void:
 	_load_all_paths()
-	#var enemy: IEnemy = enemy1.instantiate()
-	#var path: Path2D = $Paths/Path2DA1
-#
-	#IEnemySpawner.spawn_enemy(path, enemy)
 
 
+# functionnal
 func get_paths() -> Array[Path2D]:
 	return paths
 
 
+# internal
 func _load_all_paths() -> void:
-	var tmp: Array[Node] = Paths.get_children()
-	
-	for c in tmp:
-		assert(c is Path2D, "You are an idiot")
-		paths.append(c as Path2D)
+	for child in Paths.get_children():
+		assert(child is Path2D, "You are an idiot")
+		paths.append(child as Path2D)
 		
-
+		
+# signals
 func _on_timer_timeout():
-	var path: Path2D
 	if current_spawning == max_spawn:
 		return
-	if current_spawning == 0:
-		path = $Paths/Path2DA1
-	else:
-		path = $Paths/Path2DB1
-	var enemy: IEnemy = enemy1.instantiate()
+		
+	var path: Path2D = get_paths()[randi() % get_paths().size()]
+	var enemy: IEnemy = ienemy_scene.instantiate()
 	IEnemySpawner.spawn_enemy(path, enemy)
-	current_spawning += 1
-	#pass
--- scripts/enemy/enemy_spawner.gd
+++ scripts/enemy/enemy_spawner.gd
@@ -1,13 +1,23 @@
 class_name IEnemySpawner
 extends Node2D
+
+static var i: int = 0:
+	get:
+		i += 1
+		return i - 1
 	
 static func spawn_enemy(path: Path2D, enemy: IEnemy):
 	var pathfollow := PathFollow2D.new()
 	pathfollow.loop = false
 	pathfollow.rotates = false
+	
 	path.add_child(pathfollow)
 
+	enemy.name = "ENEMY_{0}".format([i])
 	enemy.path = path
 	enemy.path_follow = pathfollow
 
 	pathfollow.add_child(enemy)
+	
+	# fix same animation on multiple instance :
+	enemy.Sprite.texture = enemy.Sprite.texture.duplicate()
