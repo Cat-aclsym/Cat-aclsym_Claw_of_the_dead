5e87495df286beeff5296612b55d69df4874c508
Bahsiik <o.mist1303@gmail.com>
2024-02-28 15:06:26 +0100

    feat(IUpgrade): Update possible stats in IUpgrade.

--- scripts/upgrade/i_upgrade.gd
+++ scripts/upgrade/i_upgrade.gd
@@ -2,26 +2,33 @@ class_name IUpgrade
 extends Node2D
 
 @export var change: Dictionary = {
-									 "bullet_stat": false,
-									 "cannon_stat": false,
-									 "tower_stat": false,
-									 "cannon": false,
-									 "tower": false
-								 }
+                                     "bullet_stat": false,
+                                     "cannon_stat": false,
+                                     "tower_stat": false,
+                                     "cannon": false,
+                                     "tower": false
+                                 }
 
 @export var bulletStats: Dictionary = {
-										  "damage": 0.0,
-										  "speed": 0.0,
-									  } # {stat: float}
+                                          "damage": 0.0,
+                                          "speed": 0.0,
+                                          "piercing": 0.0,
+                                          "pierce_reduction": 0.0,
+                                          "damage_multiplier": 0.0,
+                                          "aoe_range": 0.0,
+                                          "dot_damage": 0.0,
+                                          "dot_duration": 0.0,
+                                          "dot_tick": 0.0,
+                                      } # {stat: float}
 
 @export var cannonStats: Dictionary = {
-										  "range": 0.0,
-										  "fire_rate": 0.0,
-									  } # {stat: float
+                                          "range": 0.0,
+                                          "fire_rate": 0.0,
+                                      } # {stat: float
 
 @export var towerStats: Dictionary = {
-										 "level": 0,
-									 } # {stat: float}
+                                         "level": 0,
+                                     } # {stat: float}
 
 @export var cannon: PackedScene # if a new cannon is needed
 @export var tower: PackedScene # if a new tower sprite is needed
