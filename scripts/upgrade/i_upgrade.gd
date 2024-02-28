94b839e123d49f1034b683094c084fc512895d2d
Bahsiik <o.mist1303@gmail.com>
2024-02-28 14:26:35 +0100

    feat(IUpgrade): Add IUpgrade and the first upgrade of the first tower.

--- scripts/upgrade/i_upgrade.gd
+++ scripts/upgrade/i_upgrade.gd
@@ -1,10 +1,28 @@
 class_name IUpgrade
 extends Node2D
 
+@export var change: Dictionary = {
+									 "bullet_stat": false,
+									 "cannon_stat": false,
+									 "tower_stat": false,
+									 "cannon": false,
+									 "tower": false
+								 }
 
-@export var change: Dictionary
-@export var bulletStats: Dictionary
-@export var cannon: ICannon
-@export var cannonStats: Dictionary
-@export var bulletSprite: PackedScene
-@export var towerStats: Dictionary
+@export var bulletStats: Dictionary = {
+										  "damage": 0.0,
+										  "speed": 0.0,
+									  } # {stat: float}
+
+@export var cannonStats: Dictionary = {
+										  "range": 0.0,
+										  "fire_rate": 0.0,
+									  } # {stat: float
+
+@export var towerStats: Dictionary = {
+										 "level": 0,
+									 } # {stat: float}
+
+@export var cannon: PackedScene # if a new cannon is needed
+@export var tower: PackedScene # if a new tower sprite is needed
+@export var price: int
