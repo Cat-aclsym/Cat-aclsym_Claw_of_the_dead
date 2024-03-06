bf328d57e803ba818f67a0564dbef6b44ad06119
Ayfri <pierre.ayfri@gmail.com>
2024-03-06 17:05:04 +0100

    feat(ui): Start creating ingame ui.

---
+++ scripts/menus/ingame_menu/ingame_menu.gd
@@ -0,0 +1,26 @@
+## File: menus/ingame_menu/ingame_menu.gd
+## The class for defining the behavior of the in-game menu and its components.
+##
+## Created: 06/03/2024
+
+class_name IngameMenu
+extends Node
+
+
+@onready var CoinsRichTextLabel: RichTextLabel = $VBoxContainer/CoinsRichTextLabel
+@onready var HealthRichTextLabel: RichTextLabel = $VBoxContainer/HealthRichTextLabel
+@onready var WavesRichTextLabel: RichTextLabel = $VBoxContainer/WavesRichTextLabel
+@onready var default_coins_text := CoinsRichTextLabel.text
+@onready var default_health_text := HealthRichTextLabel.text
+@onready var default_waves_text := WavesRichTextLabel.text
+
+
+func _ready() -> void:
+	print(default_coins_text)
+
+
+func _process(_delta: float) -> void:
+	pass
+	#	CoinsRichTextLabel.text = tr(default_coins_text % Stats.coins)
+	#	HealthRichTextLabel.text = tr(default_health_text % Stats.health)
+	#	WavesRichTextLabel.text = tr(default_waves_text % [Stats.waves])
