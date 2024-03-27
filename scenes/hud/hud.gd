da308a4730eec0ed3057b402f6c97771995371d5
Nimajjj <benjamin.borello@aol.com>
2024-03-27 11:56:27 +0100

    feat(ingameui): add ingame ui node to hud with base of code

--- scenes/hud/hud.gd
+++ scenes/hud/hud.gd
@@ -1,8 +1,28 @@
-extends CanvasLayer
+class_name HUD extends CanvasLayer
 
 @onready var option_menu = $HomeMenu
 @onready var menu_pause: MenuPause = $MenuPause
 @onready var levels_menu = $LevelsMenu
+@onready var in_game_menu = $InGameMenu
+
+
+# core
+func _ready() -> void:
+	Global.hud = self
+	
+
+# functionnal
+func start_level() -> void:
+	Log.info("HUD : Loading level interface")
+	in_game_menu.visible = true
+	in_game_menu.load_ui()
+	
+	
+func end_level() -> void:
+	Log.info("HUD : Unloading level interface")
+	in_game_menu.visible = false
+	in_game_menu.unload_ui()
+
 
 # signals
 func _on_pause_button_pressed():
---
+++ scenes/menus/in_game_menu/in_game_menu.gd
@@ -0,0 +1,34 @@
+extends Control
+
+var _is_ready: bool = false
+
+@onready var hp_label: Label = $VBoxContainer/HealthLabel
+@onready var money_label: Label = $VBoxContainer/MoneyLabel
+
+# core
+
+# functionnal
+func load_ui() -> void:
+	if ILevel.current_level == null:
+		Log.error("Cannot load ingame ui, ILevel.current_level = null")
+		return
+		
+	_is_ready = true
+	ILevel.current_level.connect("stats_updated", _update)
+	_update()
+
+
+func unload_ui() -> void:
+	_is_ready = false
+	ILevel.current_level.disconnect("stats_updated", _update)
+
+
+# internal
+func _update() -> void:
+	if !_is_ready:
+		Log.error("Can update ingame ui if it is not ready!")
+		return
+	
+	hp_label.text = "{0} HP".format([ILevel.current_level.health])
+	money_label.text = "{0} $".format([ILevel.current_level.coins])
+	
