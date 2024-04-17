1fe76c699997d9985e76efce10f4ee599b623682
MateoPerrotNasi <bew1432@gmail.com>
2024-04-17 11:33:29 +0200

    feat(level-menu): change font

--- scripts/menus/levels_menu/levels_menu.gd
+++ scripts/menus/levels_menu/levels_menu.gd
@@ -19,7 +19,7 @@ func _initialize() -> void:
 		buttons[button.get_child(0)] = button.get_child(1)
 
 	for bt in buttons:
-		bt.text = "{0} - {1}".format([buttons[bt].id, tr(buttons[bt].level_name)])
+		bt.text = "{0}".format([tr(buttons[bt].level_name)])
 		bt.get_parent().connect("pressed",
 		func():
 			assert(buttons[bt].level_scene != null, "Level scene is null.")
