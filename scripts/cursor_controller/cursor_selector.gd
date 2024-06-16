ed13e9450d45a47ac9c303096676475b457ee9e1
Ayfri <pierre.ayfri@gmail.com>
2024-06-16 18:42:35 +0200

    fix(map): Fix build cursor always visible.

--- scripts/cursor_controller/cursor_selector.gd
+++ scripts/cursor_controller/cursor_selector.gd
@@ -27,14 +27,15 @@ enum CURSOR_STATE {
 
 var map_ref: IMap = null
 var tm_ref: TileMap = null
-var _state := CURSOR_STATE.IDLE
+var _state: CURSOR_STATE = CURSOR_STATE.IDLE
 var _tower: ITower = null
 
-@onready var cursor = $cursor
+@onready var cursor: AnimatedSprite2D = $cursor
 
 
 func _ready() -> void:
 	Global.cursor = self
+	visible = false
 
 
 func _process(_delta: float) -> void:
@@ -44,28 +45,30 @@ func _process(_delta: float) -> void:
 func change_state(new_state: CURSOR_STATE, args: Array = []) -> void:
 	match new_state:
 		CURSOR_STATE.IDLE:
-			_state = new_state
 			trigger_state_idle.emit()
 			_state = new_state
+			visible = false
 		CURSOR_STATE.BUILD:
 			if _state != CURSOR_STATE.IDLE:
 				Log.warning("Cannot change cursor state from BUILD to IDLE")
-				return 
+				return
 			assert(args.size() == 1)
 			assert(args[0] is ITower)
-		
+
 			trigger_state_build.emit()
 			_state = new_state
+			visible = true
 			_state_build(args[0] as ITower)
-		
+
 		CURSOR_STATE.UPGRADE:
 			if _state != CURSOR_STATE.IDLE:
 				Log.warning("Cannot change cursor state from UPGRADE to IDLE")
-				return 
+				return
 			trigger_state_upgrade.emit()
 			_state = new_state
-			
-			
+			visible = true
+
+
 func _handle_state() -> void:
 	match _state:
 		CURSOR_STATE.IDLE:
@@ -86,33 +89,33 @@ func _state_build(tower: ITower = null) -> void:
 		_tower = tower.duplicate()
 		_tower.deactivate = true
 		add_child(_tower)
-	
+
 	# display cursor
 	cursor.visible = true
-	var a = tm_ref.local_to_map(get_global_mouse_position())
-	var b = tm_ref.map_to_local(a)
+	var a: Vector2i = tm_ref.local_to_map(get_global_mouse_position())
+	var b: Vector2 = tm_ref.map_to_local(a)
 	b -= Vector2(0, 8)
 	cursor.position = b
-	
+
 	# make tower ghost follow cursor
 	_tower.position = cursor.position - Vector2(0, 16)
-	
+
 	# change tower ghost color depending of placement
 	if _is_buildable(_tower.position):
 		_tower.modulate = COLOR_OK
 	else:
 		_tower.modulate = COLOR_KO
-		
+
 	# handle inputs
 	if Input.is_action_just_pressed("lmb"):
 		_build()
 	if Input.is_action_just_pressed("rmb"):
 		_cancel_build()
-	
+
 
 func _state_upgrade() -> void:
 	pass
-	
+
 
 func _cancel_build() -> void:
 	cursor.visible = false
@@ -124,22 +127,22 @@ func _cancel_build() -> void:
 func _build() -> void:
 	if !_is_buildable(_tower.position):
 		return
-		
+
 	var new_tower: ITower = _tower.duplicate()
 	new_tower.deactivate = false
 	new_tower.modulate = Color(1, 1, 1, 1)
 	map_ref.add_child(new_tower)
-	
+
 	ILevel.current_level.coins -= _tower.cost
-	
+
 	_cancel_build()
-	
+
 
 func _is_buildable(pos: Vector2) -> bool:
 	if ILevel.current_level.coins < _tower.cost:
 		return false
-		
+
 	#var atlas_coords: Vector2 = tm_ref.get_cell_atlas_coords(0, tm_ref.local_to_map(pos))
-	#Log.debug("atlas coords = {0}; buildable = {1}".format([atlas_coords, true]))	
-		
+	#Log.debug("atlas coords = {0}; buildable = {1}".format([atlas_coords, true]))
+
 	return true
