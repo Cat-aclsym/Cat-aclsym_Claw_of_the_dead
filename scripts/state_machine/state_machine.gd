f3a940093d35cdd1167a3c974d2a0d4a7b896fb7
Laurie <laurie.jeham@hotmail.fr>
2024-12-18 16:53:18 +0100

    doc(state machine) : Add doc for the state machine

--- scripts/state_machine/state_machine.gd
+++ scripts/state_machine/state_machine.gd
@@ -1,11 +1,15 @@
+## © [2024] A7 Studio. All rights reserved. Trademark.
+## This is the StateMachine class which extends from Object
+## It's used to control the different states in our game
 class_name StateMachine extends Object
 
+## Signals that is emitted when the state is changed, sending the new sqtate as the argiument
 signal state_changed(State)
 
-var name: String
-var states: Dictionary # [String => State]
-var valid_transition: Array[Transition]
-var current_state: State
+var name: String #name of the state machine
+var states: Dictionary # [String => State] mapping of the state IDs to states
+var valid_transition: Array[Transition] # List of valid transitions
+var current_state: State # Current state of the state machine
 
 
 # core
@@ -16,7 +20,9 @@ func _init() -> void:
 	current_state = null
 
 
-# public
+## Method used to change the state
+## [param state_id] : ID of the chosen state
+## It will validate the transition, and if var id, execute it and emit a state_changed signal
 func toggle_state(state_id: String) -> bool:
 	if not state_id in states.keys():
 		Log.trace(Log.Level.ERROR, "%s : Unkown state id : '%s'" % [name, state_id])
@@ -33,33 +39,41 @@ func toggle_state(state_id: String) -> bool:
 	Log.trace(Log.Level.ERROR, "%s : Failed to toggle state %s : invalid transition from %s to %s" % [name, state_id, current_state, states.get(state_id)])
 	return false
 
-
+## This is used at the start of the game to set the initial state
 func toggle_initial_state() -> bool:
 	assert(current_state, "Initial state has not be initialized")
 	Log.trace(Log.Level.DEBUG, "%s : Toggle initial state %s" % [name, current_state])
 	state_changed.emit(current_state)
 	return true
 
-
+## This is used to interact with the current state
 func handle_current_state(args: Array) -> bool:
 	return current_state.handle(args)
 
-
+## Getter for current_state
 func get_current_state() -> State:
 	return current_state
 
-
+## Public method to add new stated to the state machine
+##
+## [param state_id] : ID of the state
+## [param new_state] : new state to add
 func add_state(state_id: String, new_state: State) -> void:
 	assert(not state_id in states.keys(), "%s is already registered" % new_state)
 	Log.trace(Log.Level.INFO, "%s : New state %s" % [name, new_state])
 	states[state_id] = new_state
 
-
+# Public
+## Method to add initial state to the state machine
+## [param state_id] : ID of the state
+## [param new_state] : new state to add
 func add_initial_state(state_id: String, new_state: State) -> void:
 	add_state(state_id, new_state)
 	current_state = states.get(state_id)
 
-
+# Public
+## Method to add a new transition ti the state machine
+## [param new _transition] : new Transition Object
 func add_transition(new_transition: Transition) -> void:
 	assert(not _is_transition_valid(new_transition.from, new_transition.to), "%s is already registered" % new_transition)
 	Log.trace(Log.Level.INFO, "%s : New transition %s" % [name, new_transition])
@@ -67,13 +81,18 @@ func add_transition(new_transition: Transition) -> void:
 
 
 # private
+## This method check if a transition exists between the given states
+##
+## [param State] : State Object
 func _is_transition_valid(from: State, to: State) -> bool:
 	if _find_transition(from, to):
 		return true
 
 	return false
 
-
+## This methods finds a transition object between two states
+##
+## [param State] : State Object
 func _find_transition(from: State, to: State) -> Transition:
 	for tra in valid_transition:
 		if tra.from == from and tra.to == to:
--- scripts/state_machine/transition.gd
+++ scripts/state_machine/transition.gd
@@ -1,20 +1,27 @@
+## © [2024] A7 Studio. All rights reserved. Trademark.
+## The Transition class represents a transition between two states in a StateMachine
 class_name Transition extends Object
 
-var from: State
-var to: State
-var callback: Callable
+var from: State # The state that this transition will progress from
+var to: State # The state that this transition will advance to
+var callback: Callable # A Callable object which will be executed when the transition is performed
 
 # core
+## The constructor for the Transition class
+## [param new_from] : The 'from' state for this transition
+## [param new_to] : The 'to' state for this transition
+## [param new_callback] : A Callable object which will be executed when this transition is performed
 func _init(new_from: State, new_to: State, new_callback: Callable) -> void:
 	from = new_from
 	to = new_to
 	callback = new_callback
 
-
+## Returns a string representation of the Transition for debugging purposes
 func _to_string() -> String:
 	return "Transition(%s => %s: %s)" % [from, to, callback]
 
-
+## Performs the transition
+## Executes the callback associated with this transition
 func handle() -> void:
 	callback.call()
 
