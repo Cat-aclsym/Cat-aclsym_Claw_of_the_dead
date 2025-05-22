6afb84de50a589adbaf34c8127d880d1af403371
Nimajjj <benjamin.borello@aol.com>
2025-05-22 12:41:44 +0200

    feat(level-selection): add level indicators

--- scenes/ui/menus/level_selection/components/level_indicator.gd
+++ scenes/ui/menus/level_selection/components/level_indicator.gd
@@ -1,9 +1,12 @@
 class_name LevelIndicator extends TextureButton
 
+signal selected(LevelIndicator)
+
 enum Status {
 	CURRENT,
 	COMPLETED,
-	LOCKED
+	LOCKED,
+	ERROR
 }
 
 var status := LevelIndicator.Status.CURRENT
@@ -17,6 +20,10 @@ var textures: Dictionary = {
 
 @onready var label: Label = $Label
 
+@onready var signals: Array[Dictionary] = [
+	{SignalUtil.WHO: self, SignalUtil.WHAT: "pressed", SignalUtil.TO: _on_pressed}
+]
+
 # core
 
 
@@ -24,8 +31,9 @@ var textures: Dictionary = {
 func configure(in_level: int, in_status: LevelIndicator.Status) -> void:
 	level = in_level
 	status = in_status
+	SignalUtil.connects(signals)
 	update()
-	
+
 
 func update() -> void:
 	label.text = str(level)
@@ -33,11 +41,22 @@ func update() -> void:
 	if not disabled:
 		texture_normal = textures[status]
 
+static func string_to_status(s: String) -> LevelIndicator.Status:
+	match s:
+		"current":
+			return LevelIndicator.Status.CURRENT
+		"done":
+			return LevelIndicator.Status.COMPLETED
+		"locked":
+			return LevelIndicator.Status.LOCKED
+	return LevelIndicator.Status.ERROR
+
 # private
 
 
 # signal
-
+func _on_pressed() -> void:
+	selected.emit(self)
 
 # event
 
--- scenes/ui/menus/level_selection/level_selection_menu.gd
+++ scenes/ui/menus/level_selection/level_selection_menu.gd
@@ -3,11 +3,12 @@ class_name LevelSelectionMenu extends Control
 
 signal level_selected
 
+var level_statuses: Dictionary = {}
 var level_frames: Array[LevelFrame] = []
 var level_index: int = 0
 
 @onready var separator_scene: PackedScene = preload("res://scenes/ui/menus/level_selection/components/separator.tscn")
-@onready var indicator_scene: PackedScene = preload("res://scenes/ui/menus/level_selection/components/level_frame.tscn")
+@onready var indicator_scene: PackedScene = preload("res://scenes/ui/menus/level_selection/components/level_indicator.tscn")
 
 @onready var background_texture_rect: TextureRect = $BackgroundTextureRect
 
@@ -16,6 +17,8 @@ var level_index: int = 0
 @onready var body_container: HBoxContainer = $MarginContainer/VBoxContainer/BodyContainer
 @onready var previous_button: TextureButton = $MarginContainer/VBoxContainer/BodyContainer/LeftCenterContainer/PreviousButton
 @onready var next_button: TextureButton = $MarginContainer/VBoxContainer/BodyContainer/RightCenterContainer/NextButton
+
+@onready var indicators_container: HBoxContainer = $MarginContainer/VBoxContainer/FooterContainer/VBoxContainer/IndicatorsContainer
 
 @onready var signals: Array[Dictionary] = [
 	{SignalUtil.WHO: previous_button, SignalUtil.WHAT: "pressed", SignalUtil.TO: _on_previous_button_pressed},
@@ -36,10 +39,16 @@ func configure() -> void:
 
 # private
 func _update() -> void:
+	for level_frame in level_frames:
+		level_frame.visible = false
 	level_frames[level_index].visible = true
 	background_texture_rect.texture = level_frames[level_index].arc_texture
 
 func _load_levels() -> void:
+	var i := 1
+
+	level_statuses = _load_level_statuses()
+
 	for child in body_container.get_children():
 		if not child is LevelFrame:
 			continue
@@ -49,6 +58,45 @@ func _load_levels() -> void:
 			SignalUtil.WHAT: "start_level", 
 			SignalUtil.TO: _on_frame_start_level
 		})
+
+		var indicator: LevelIndicator = indicator_scene.instantiate()
+		indicators_container.add_child(indicator)
+		indicator.configure(i, level_statuses[child.level_id])
+		SignalUtil.connects([{SignalUtil.WHO: indicator, SignalUtil.WHAT: "selected", SignalUtil.TO: _on_level_indicator_selected}])
+
+		var separator := separator_scene.instantiate()
+		indicators_container.add_child(separator)
+
+		i += 1
+
+	indicators_container.get_children().back().queue_free()
+
+func _load_level_statuses() -> Dictionary:
+	var result := {}
+	var dir := DirAccess.open("res://resources/levels")
+	
+	if dir:
+		dir.list_dir_begin()
+		var file_name := dir.get_next()
+		
+		while file_name != "":
+			if not dir.current_is_dir() and file_name.begins_with("lev.") and file_name.ends_with(".json"):
+				var name_no_ext := file_name.get_basename()  # removes .json
+				var file := FileAccess.open("res://resources/levels/%s" % file_name, FileAccess.READ)
+				if file:
+					var json_text := file.get_as_text()
+					var json = JSON.parse_string(json_text)
+					if typeof(json) == TYPE_DICTIONARY and json.has("status"):
+						result[name_no_ext] = LevelIndicator.string_to_status(json["status"])
+			file_name = dir.get_next()
+		
+		dir.list_dir_end()
+	else:
+		Log.trace(Log.Level.ERROR, "Could not open directory: %s" % dir);
+
+
+	return result
+
 
 
 # signal
@@ -64,22 +112,31 @@ func _on_frame_start_level(level: ILevel) -> void:
 
 
 func _on_previous_button_pressed() -> void:
-	level_frames[level_index].visible = false
 	if level_index > 0:
 		level_index -= 1
 	_update()
 
 
 func _on_next_button_pressed() -> void:
-	level_frames[level_index].visible = false
+	var i = (level_index + 1) if level_index + 1 < level_frames.size() else level_index
+	var id = level_frames[i].level.level_id
+	if level_statuses[id] == LevelIndicator.Status.LOCKED:
+		return
+
 	if level_index < level_frames.size() - 1:
 		level_index += 1
+
 	_update()
 
 
 func _on_main_menu_button_button_pressed() -> void:
 	get_parent().gui_margin_container.visible = true
 	queue_free()
+
+
+func _on_level_indicator_selected(indicator: LevelIndicator) -> void:
+	level_index = indicator.level - 1
+	_update()
 
 # event
 
