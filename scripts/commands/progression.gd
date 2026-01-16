0039fa9e5b7e130570cd489da76889364a77b444
Ayfri <pierre.ayfri@gmail.com>
2026-01-16 10:08:32 +0100

    feat(progression): Add progression show sub-command to display json.

--- scripts/commands/progression.gd
+++ scripts/commands/progression.gd
@@ -1,4 +1,4 @@
-## © [2024] A7 Studio. All rights reserved. Trademark.
+## © [2026] A7 Studio. All rights reserved. Trademark.
 ##
 ## Manages game progression via console.
 extends ICommand
@@ -6,12 +6,12 @@ extends ICommand
 
 # Public functions
 func description() -> String:
-	return "Manages game progression, allowing to reset, unlock levels, or complete challenges."
+	return "Manages game progression, allowing to reset, unlock levels, complete challenges, or show data."
 
 
 func get_args() -> Array[Dictionary]:
 	return [
-		{"name": "action", "type": Types.ARG_ENUM, "enum_values": ["reset", "unlock", "challenge"]},
+		{"name": "action", "type": Types.ARG_ENUM, "enum_values": ["challenge", "reset", "show", "unlock"]},
 		{"name": "level_id", "type": Types.ARG_LEVEL, "optional": true},
 		{"name": "challenge_id", "type": Types.ARG_STRING, "optional": true}
 	]
@@ -26,10 +26,21 @@ func _execute(console: Console, args: Array) -> int:
 	var subcommand: String = args[0]
 
 	match subcommand:
+		"challenge":
+			if args.size() < 3:
+				console.push_error("Usage: progression challenge <level_id> <challenge_id>")
+				return OK
+			var level_id: String = args[1]
+			var challenge_id: String = args[2]
+			ProgressionManager.complete_challenge(level_id, challenge_id)
+			console.push_text("Completed challenge " + challenge_id + " for level " + level_id)
 		"reset":
 			ProgressionManager.reset_progression()
 			console.push_text("Progression reset.")
-
+		"show":
+			var data: Dictionary = ProgressionManager.data.save()
+			var json_text: String = JSON.stringify(data, "\t")
+			console.push_text(json_text)
 		"unlock":
 			if args.size() < 2:
 				console.push_error("Usage: progression unlock <level_id>")
@@ -40,16 +51,6 @@ func _execute(console: Console, args: Array) -> int:
 			ProgressionManager.data.levels[level_id].unlocked = true
 			ProgressionManager.save_game()
 			console.push_text("Unlocked level: " + level_id)
-
-		"challenge":
-			if args.size() < 3:
-				console.push_error("Usage: progression challenge <level_id> <challenge_id>")
-				return OK
-			var level_id: String = args[1]
-			var challenge_id: String = args[2]
-			ProgressionManager.complete_challenge(level_id, challenge_id)
-			console.push_text("Completed challenge " + challenge_id + " for level " + level_id)
-
 		_:
 			console.push_error("Unknown subcommand: " + subcommand)
 
--- scripts/progression/progression_data.gd
+++ scripts/progression/progression_data.gd
@@ -16,3 +16,25 @@ var parameters: SettingsData = SettingsData.new()
 
 ## Dictionary mapping tower IDs (String) to TowerData objects.
 var towers: Dictionary = {}
+
+
+# Public functions
+## Converts the entire progression to a dictionary for serialization or debugging.
+func save() -> Dictionary:
+	var res: Dictionary = {}
+
+	res["enemies"] = {}
+	for id in enemies:
+		res["enemies"][id] = enemies[id].save()
+
+	res["levels"] = {}
+	for id in levels:
+		res["levels"][id] = levels[id].save()
+
+	res["parameters"] = parameters.save()
+
+	res["towers"] = {}
+	for id in towers:
+		res["towers"][id] = towers[id].save()
+
+	return res
