f14eff5815db3cd435d02c55455cc8ed7c552428
Dxvy <55587711+Dxvy@users.noreply.github.com>
2026-04-08 11:35:44 +0200

    Update wave_step_spawn.gd

--- scripts/wave/wave_step_spawn.gd
+++ scripts/wave/wave_step_spawn.gd
@@ -10,8 +10,6 @@ func _init(in_data: Dictionary) -> void:
 
 # public
 ## Executes the spawn logic, instantiating an enemy and placing it on a path.
-
-
 func exec() -> void:
 	var enemy_id: String = _data[WaveStep.ENEMY_ID]
 	var spawner_index: int = _data[WaveStep.SPAWNER]
