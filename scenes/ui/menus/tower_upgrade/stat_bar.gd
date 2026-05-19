a903080b28f0f9365dd67a312a9766724bfc1f2b
Ayfri <pierre.ayfri@gmail.com>
2026-05-19 17:10:25 +0200

    chore: sync Godot-generated UIDs, fix whitespace and level indentation

--- scenes/ui/menus/tower_upgrade/stat_bar.gd
+++ scenes/ui/menus/tower_upgrade/stat_bar.gd
@@ -13,14 +13,14 @@ func setup(stat_name: String, current_value: float, new_value: float, icon: Text
 	# Set icon
 	if icon != null:
 		_icon.texture = icon
-	
+
 	# Format and set stat name
 	_stat_name_label.text = _format_stat_name(stat_name)
-	
+
 	# Set progress bar value
 	var normalized: float = clamp(new_value / max_value, 0.0, 1.0)
 	_progress_bar.value = normalized * max_value
-	
+
 	# Set tooltip
 	var tooltip: String
 	if show_change:
@@ -34,9 +34,9 @@ func _format_stat_name(stat_name: String) -> String:
 	# Convert snake_case to Title Case
 	var words: PackedStringArray = stat_name.split("_")
 	var formatted_words: Array[String] = []
-	
+
 	for word in words:
 		if word.length() > 0:
 			formatted_words.append(word.capitalize())
-	
+
 	return " ".join(formatted_words)
